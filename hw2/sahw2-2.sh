bd22db983726bad2f10991c66cb94489fdb06b27
wlshih <wl.shih2010@gmail.com>
2019-10-19 14:04:20 +0800

    text edit feature added

--- hw2/sahw2-2.sh
+++ hw2/sahw2-2.sh
@@ -17,10 +17,11 @@ MEM="/tmp/.mem.sahw2.tmp"
 NET="/tmp/.net.sahw2.tmp"
 FILE="/tmp/.file.sahw2.tmp"
 
-
+# actually the source directory
+SOURCE=$(pwd)
 
 cpuInfo() {
-	sysctl hw.model | awk '{ print "CPU model: " $2 }' > $TMP
+	sysctl hw.model | awk '{ $1 = ""; print "CPU model: " $0 }' > $TMP
 	sysctl hw.machine | awk '{ print "CPU machine: " $2 }' >> $TMP
 	sysctl hw.ncpu | awk '{ print "CPU core: " $2 }' >> $TMP
 
@@ -32,18 +33,18 @@ cpuInfo() {
 		# show dialog and calculate percetage on the second iteration	
 		if [ $count -ne 0 ]; then
 			tail -n $count $CPU | awk -F'[\ %]' -v cnt=$count '{ idle += $9 } END{ print int(100-idle/cnt) }' | \
-			dialog	--title "CPU INFO" --gauge "$(cat $CPU)" 17 50
+			dialog	--title "CPU INFO" --gauge "$(cat $CPU)" 17 60
 		else
-			echo 0 | dialog --title "CPU INFO" --gauge "$(cat $CPU)" 17 50
+			echo 0 | dialog --title "CPU INFO" --gauge "$(cat $CPU)" 17 60
 		fi
 			
 		cat $TMP > $CPU
 		top -Pd2 | grep ^CPU | tail -n2 | awk '{ print $1$2" USER: "$3" SYST: "$7" IDLE: "$11 }' >> $CPU
 		
-		# calculate percentage
+		# count lines = number of CPUs
 		count=$(cat $CPU | wc -l)-5
 
-		# read input, timeout = 0.5 sec.
+		# read input
 		read -t 1 -N 1 input
 		if [ $? -eq 0 ] && [ -z $input ]; then # if exit status = 0, and input length is zero (-z)
 			#mainMenu
@@ -71,7 +72,6 @@ memInfo() {
 
 		# string handling
 		text=$(awk -v ORS='\\n' '1' $TMP)
-		#echo $text
 	
 		echo $percent | dialog	--title "Memory Info and Usage" --gauge "$text" 17 40 
 		
@@ -116,11 +116,10 @@ netInfo() {
 
 fileBrowser() 
 {
-	echo file
 	# get full path
 	path=$(readlink -f $1)
 	
-	# ls and get MIME type
+	# cd then ls and get MIME type
 	cd $path 2> /dev/null
 	ls -a | awk '{ printf $1 " "; system("file " $1 " --mime-type -b") }' > $FILE
 	dialog	--title "File Browser" --menu "$path" 17 50 12\
@@ -130,9 +129,9 @@ fileBrowser()
 	option=$(cat $TMP)
 
 	if [ $result -eq $DIALOG_OK ]; then
-		dir=$(cat $FILE | grep $option -w | awk '{ if($2 ~ /^inode/) print }')
-		echo $dir
-		echo $option	
+		dir=$(cat $FILE | grep $option -w | awk '{ if($2 ~ /^inode/) print }') # $dir will not be empty if the object is a inode
+		#echo $dir
+		#echo $option	
 		if [[ ! -z $dir ]]; then
 			fileBrowser $option
 		else
@@ -150,8 +149,20 @@ fileBrowser_info()
 	printf "<File Info>: $(file $1 -b)\n" >> $TMP 
 	ls -l $1 | awk '{ print $5 }' | unitConvert | awk '{ print "<File Size>: " $1 " " $2 }' >> $TMP
 
-	dialog --title "File Browser" --msgbox "$(cat $TMP)" 17 50
-	fileBrowser $(readlink -f $(dirname $1))
+	# determine if it is a text file
+	text=$(cat $FILE | grep $option -w | awk '{ if($2 ~ /text/) print }')
+	if [[ -z $text ]]; then
+		dialog --title "File Browser" --msgbox "$(cat $TMP)" 17 50
+	else
+		dialog --title "File Browser" --yes-label "Ok" --no-label "Edit" --yesno "$(cat $TMP)" 17 50 
+	fi
+
+	result=$?
+	if [ $result -ne $DIALOG_OK ]; then
+		$EDITOR $1
+	fi
+
+	fileBrowser $(pwd)
 }
 
 # use pipe to connect input output
@@ -193,7 +204,8 @@ mainMenu() {
 		elif [ $option == "3" ]; then
 			netInfo
 		else
-			fileBrowser ./
+			echo test
+			fileBrowser $SOURCE
 		fi
 	else
 		exit 0
